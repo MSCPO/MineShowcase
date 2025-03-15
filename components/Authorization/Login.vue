4e28322daf6006bb355b28ffe8f17d79852b5f77
MSCMDD <MSCAoMe@outlook.com>
2025-03-15 23:47:00 +0800

    refactor: 调整认证组件

--- components/Authorization/Login.vue
+++ components/Authorization/Login.vue
@@ -118,8 +118,8 @@ const handleSubmit = async () => {
                             登录
                         </a-button>
                     </reCaptcha>
-                    <a-button v-else type="primary" :loading="true">
-                        加载中...
+                    <a-button v-else type="primary" loading>
+                        登录
                     </a-button>
                 </div>
             </a-col>
